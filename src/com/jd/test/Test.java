8b443d772717444d9e83883e683cf27439544d23
my0811 <1368674626@qq.com>
2016-07-27 14:15:51 +0800

    我的第一个git 发布！

---
+++ src/com/jd/test/Test.java
@@ -0,0 +1,11 @@
+package com.jd.test;
+
+public class Test {
+	
+	public static void main(String[] args) {
+		System.out.println("hello jd.com");
+		String a ="yangzhongkui";
+		System.out.println(a);
+	}
+
+}
